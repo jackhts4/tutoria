18ecf30a66ea73e900b8597787de02e1e8dbfd30
jackhts4 <33593518+jackhts4@users.noreply.github.com>
2017-11-12 22:09:52 +0800

    Create registration.py

---
+++ tutors/registration.py
@@ -0,0 +1,17 @@
+from django.contrib import auth  # 別忘了import auth
+
+def login(request):
+
+    if request.user.is_authenticated(): 
+        return HttpResponseRedirect('/index/')
+
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render_to_response('login.html') 
